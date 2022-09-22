eeb019e1e9123fc4834c27e10ac1f8ade613280c
Mphan87 <102190333+Mphan87@users.noreply.github.com>
2022-09-22 08:31:12 -0700

    Add files via upload

--- src/DeckViewer.js
+++ src/DeckViewer.js
@@ -1,40 +1,25 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios"
 
-
 const DeckViewer = () => {
     const [deck, setDeck] = useState(null);
     const [card, setCard] = useState();
 
-
     useEffect(() => {
+        
         async function getData() {
             let d = await axios.get(`http://deckofcardsapi.com/api/deck/new/shuffle/`);
             setDeck(d.data);
-            let deck_id = deck.deck_id
+            let deck_id = d.data.deck_id
             let cardRes = await axios.get(`http://deckofcardsapi.com/api/deck/${deck_id}/draw/`);
             const randomCard = cardRes.data.cards[0]
             setCard(randomCard.image)
         }
         getData();
     }, [setDeck]);
- 
 
-
-    // useEffect(() => {
-    //     async function getCard() {
-    //         let deck_id = deck.deck_id
-
-    //         let cardRes = await axios.get(`http://deckofcardsapi.com/api/deck/${deck_id}/draw/`);
-    //         const randomCard = cardRes.data.cards[0]
-    //         setCard(randomCard.image)
-
-    //     }
-    //     getCard();
-    // }, [setCard]);
-
-
-    const handleClicker = (e) => {
+    
+    const handleClickerRandom = (e) => {
         e.preventDefault();
         async function newCard() {
             let deck_id = deck.deck_id
@@ -42,13 +27,31 @@ const DeckViewer = () => {
             const randomCard = cardRes.data.cards[0]
             setCard(randomCard.image)
         }
-        newCard();
+       setInterval(newCard,5000)
     }
+
+
+    const handleClickerCard = (e) => {
+        e.preventDefault();
+        async function newCard() {
+            let deck_id = deck.deck_id
+            let cardRes = await axios.get(`http://deckofcardsapi.com/api/deck/${deck_id}/draw/`);
+            const randomCard = cardRes.data.cards[0]
+            setCard(randomCard.image)
+        }
+       newCard()
+    }
+
+
+
+    // let CardInterval = setInterval(handleClicker, 1000)
+
 
     return (
         <h3>  
             <img src={card} /> 
-            <button onClick={handleClicker}>New Card</button>
+            <button onClick={handleClickerCard}>New Card</button>
+            <button onClick={handleClickerRandom}>Draw Random Card</button>
         </h3>
     )
 }
